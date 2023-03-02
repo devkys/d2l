0584667e3cc5d0542f249722274ec38bd7465b62
kyeongseo <keongseo1234@gmail.com>
2023-03-02 21:13:17 +0900

    neuron.py

--- neuron.py
+++ neuron.py
@@ -1,8 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
 
 X = np.array([1.0, 0.5])
 W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
@@ -13,6 +9,31 @@ print(X.shape)
 print(B1.shape)
 
 A1 = np.dot(X, W1) + B1
+print(A1)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
 Z1 = sigmoid(A1)
+
 print(A1)
 print(Z1)
+
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3,0.6]])
+B2 = np.array([0.1, 0.2])
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+# 출력함수(항등함수 사용)
+def identity_function(x):
+    return x
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y= identity_function(A3)
+
+print("Y = " , Y)
